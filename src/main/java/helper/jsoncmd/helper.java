8a6b1915fbf6ce91ed44fedb59dbe9c755c421bd
JSONCMD <develnet.info@gmail.com>
2021-12-02 09:10:45 -0500

    sync app

---
+++ src/main/java/helper/jsoncmd/helper.java
@@ -0,0 +1,17 @@
+package helper.jsoncmd;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class helper extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
